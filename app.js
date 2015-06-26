438b2b35dc0b4b48c7a783a27c6ae69555fe7483
Zach Fountain <zach.fountain@gmail.com>
2015-06-26 12:43:19 -0500

    Added mongodb - haven't set up db yet

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ var users = require('./routes/users');
 
 var app = express();
 
+// Mongo DB connection
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+ 
+// Connection URL 
+var url = 'mongodb://localhost:27017/Corgi-Tinder';
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+ 
+  db.close();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
